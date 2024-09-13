f6315a38915e3a38788fc2cc2aff752cca5afa72
43Goose <goose.dvlpr@gmail.com>
2024-09-12 23:08:48 -0700

    connection error potential attempt

--- lib/socket.ts
+++ lib/socket.ts
@@ -3,5 +3,6 @@
 import io from 'socket.io-client';
 
 const port = process.env.PORT || 3000;
+const url = process.env.NODE_ENV === 'production' ? 'https://footprint-v3w8.onrender.com' : `http://localhost:${port}`;
 
-export const socket = io(`http://localhost:${port}`);
+export const socket = io(url);
